78172a2524f0f3d3a1b52cea4462f41538f78903
cyan5 <r.vanis2000@gmail.com>
2023-06-16 03:35:17 +0900

    initial add file

---
+++ numer0n_call/c_func.h
@@ -0,0 +1,47 @@
+#ifndef CFUNC
+#define CFUNC
+
+#define USENUM 10      /* 使用する記号の数             */
+#define SIZE 720       /* USENUM*(USENUM-1)*(USENUM-2) */
+#define HIST 10
+
+enum type{
+    DIV,      /* 全てを区別する。notを返す */
+    NEU,      /* 全て区別しない            */
+    LM,       /* LとMは区別しない          */
+    LR,       /* LとRは区別しない          */
+    MR        /* MとRは区別しない          */
+};
+
+typedef struct hash{
+    int data[SIZE];
+    struct hash *next;
+}hash;
+/* HIST個のハッシュ値をリストとして保存しておく
+ * リストは全て等しい場合のみ、同一の質問候補として扱われる
+ * */
+
+int calc_idx(int cand[3], int call_hist[3], int type);
+/* 質問履歴から、次の質問候補をハッシュ値で返す
+ * 012→013  hash:1  012→014  hash:2  ...  012→345  hash:34 */
+
+int calc_type(int call[3], int call_hist[3], int type);
+/* 受け取った質問と質問履歴から、タイプを更新する。
+   012→013 : type 0→1  
+ * typeを受け取り、値を更新する */
+
+int hash_search(hash *ptr, int data[HIST], int depth);
+/* ハッシュ値配列 data[HIST] が 単方向リストhashに含まれているか検索
+ * 含まれている場合:0  含まれていない場合:1 */
+
+void hash_push(hash **ptr, int data[HIST]);
+// void hash_push(hash **ptr, int data[HIST], int type);
+/* 単方向リストhashにデータをプッシュ */
+
+void hash_pop(int *type_stb, int type_tmp);
+/* 現在のタイプと新しいタイプからタイプを決定 */
+
+void hash_clear(hash **ptr);
+/* 単方向リストhashを空にする */
+
+#endif
---
+++ numer0n_call/candidate.c
@@ -0,0 +1,94 @@
+/* candidate.c
+ * cyan5   */
+
+#include <stdio.h>
+#include "c_func.h"
+
+int main(void){
+
+    /* 引数 */
+    int call_lst[SIZE*3];
+    int call_hist[HIST*3];
+
+    /* 候補リストの作成 */
+    int cand_oridin[SIZE*3];
+    int idx_cand = 0;
+    for(int i=0; i<10; i++){
+        for(int j=0; j<10; j++){
+            if(i != j){
+                for(int k=0; k<10; k++){
+                    if(k != i && k != j){
+                        cand_oridin[idx_cand*3  ] = i;
+                        cand_oridin[idx_cand*3+1] = j;
+                        cand_oridin[idx_cand*3+2] = k;
+                        idx_cand++;
+                    }
+                }
+            }
+        }
+    }
+
+    /* 関数内で定義 */
+
+    int idx;
+    int stop;
+    int flag_zero;
+    hash *hash_lst = NULL;
+    int idx_lst[HIST];
+    int type = NEU;
+
+    for(int depth=0; depth<HIST; depth++){
+
+        /* 数字の入力 */
+        printf("start sequence %d\n", depth);
+        printf("num 0 1 2 ? > ");
+        scanf("%d %d %d", &call_hist[depth*3], &call_hist[depth*3+1], &call_hist[depth*3+2]);
+
+        /* 入力した数字を出力 */
+        printf("%d%d%d\n", call_hist[depth*3], call_hist[depth*3+1], call_hist[depth*3+2]);
+
+        /* typeを更新 すべてのdepthと比較。DIVが出てきたらDIVで確定なのでbreak
+         * 最初がtype == DIV (0) のとき更新の必要なし */
+        if(type){
+            for(int i=0; i<depth; i++){
+                type = calc_type(&call_hist[depth*3], &call_hist[i*3], type);
+                if(type == DIV){
+                    break;
+                }
+            }
+        }
+
+        /* すべての候補について走査 */
+        idx = 0;
+        for(int i=0; i<SIZE; i++){
+
+            /* 質問候補のハッシュが0(=同じ質問)の排除 */
+            flag_zero = 1;
+            for(int j=0; j<=depth; j++){
+                idx_lst[j] = calc_idx(&cand_oridin[i*3], &call_hist[j*3], type);
+                if(idx_lst[j] == 0){
+                    flag_zero = 0;
+                    break;
+                }
+            }
+
+            if(flag_zero && hash_search(hash_lst, idx_lst, depth)){
+                hash_push(&hash_lst, idx_lst);
+                call_lst[idx*3  ] = cand_oridin[i*3  ];
+                call_lst[idx*3+1] = cand_oridin[i*3+1];
+                call_lst[idx*3+2] = cand_oridin[i*3+2];
+                idx++;
+            }
+        }
+
+        printf("\n");
+        for(int i=0; i<idx; i++){
+            printf("%d%d%d ", call_lst[i*3], call_lst[i*3+1], call_lst[i*3+2]);
+        }
+        hash_clear(&hash_lst);
+        printf("\n");
+    }
+
+    scanf("%d", &stop);
+    return 0;
+}
